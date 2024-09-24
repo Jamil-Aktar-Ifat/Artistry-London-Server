42ab50c6af3cfdfd42440579dc761cfe6fad14e7
ifat <i>
2024-09-24 17:51:14 +0100

    craft update funtionality added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5005;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // middleware
 app.use(cors());
@@ -26,16 +26,59 @@ async function run() {
     await client.connect();
     const craftsCollection = client.db("craftsdb").collection("craft");
 
+    // get to ui
     app.get("/crafts", async (req, res) => {
       const cursor = craftsCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
 
+    // Getting specific coffee data
+    app.get("/crafts/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await craftsCollection.findOne(query);
+        if (result) {
+          res.send(result);
+        } else {
+          res.status(404).send({ error: "Coffee not found" });
+        }
+      } catch (error) {
+        res.status(500).send({ error: error.message });
+      }
+    });
+
+    // post to db
     app.post("/crafts", async (req, res) => {
       const newCraft = req.body;
       console.log(newCraft);
       const result = await craftsCollection.insertOne(newCraft);
+      res.send(result);
+    });
+
+    // update item
+    app.put("/crafts/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedCraft = req.body;
+      const craft = {
+        $set: {
+          item_name: updatedCraft.item_name,
+          subcategory_name: updatedCraft.subcategory_name,
+          short_description: updatedCraft.short_description,
+          price: updatedCraft.price,
+          rating: updatedCraft.rating,
+          Customisation: updatedCraft.Customisation,
+          processing_time: updatedCraft.processing_time,
+          stock_status: updatedCraft.stock_status,
+          user_email: updatedCraft.user_email,
+          user_name: updatedCraft.user_name,
+          imageURL: updatedCraft.imageURL,
+        },
+      };
+      const result = await craftsCollection.updateOne(filter, craft, options);
       res.send(result);
     });
 
