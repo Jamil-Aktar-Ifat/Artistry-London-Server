bfa746d6f7a136ec4450a5e489bb6769d9d98f28
ifat <i>
2024-09-27 16:59:54 +0100

    vercel installed

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server (optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const craftsCollection = client.db("craftsdb").collection("craft");
 
     // Get crafts (with optional filtering by user email)
@@ -105,7 +105,7 @@ async function run() {
     });
 
     // Ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
